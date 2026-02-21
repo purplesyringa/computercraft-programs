f22dfb6dd70bfc47690804ec429b4bc18247e1c0
Alisa Sireneva <me@purplesyringa.moe>
2026-02-21 17:32:11 +0300

    rsh: Make rshd compatible with svc

--- sys/packages/rsh/bin/rshd.lua
+++ sys/packages/rsh/bin/rshd.lua
@@ -1,71 +1,36 @@
 local async = require "async"
 local named = require "named"
 local remote_events = require("rsh.events").remote_events
+local svc = require "svc"
 local vt = require "rsh.vt"
 
 local function startProgram(program, ...)
-    -- Even if we want to run a specific command, we need to run it inside a shell to give it
-    -- an environment. But running `shell <program>` doesn't invoke the ROM startup script, which is
-    -- actually responsible for setting up the environment, so we have to invoke ourselves as
-    -- a proxy to invoke the ROM startup script first and the requested command second.
-    local command = {}
-    if program ~= nil then
-        command = { shell.getRunningProgram(), "--serve", program, ... }
-    end
-
-    -- The ROM startup script not only sets up the environment, but also runs MOTD and startup
-    -- scripts from disks and the filesystem root, so we have to monkey-patch `settings.get` to
-    -- disable that behavior.
-    local one_time_overrides = {
+    local nested_shell = svc.makeNestedShell({ shell = shell })
+    -- The shell PATH changes depending on whether the connected terminal is advanced, which can
+    -- change across network, so we need to reinitialize it. The ROM startup script is responsible
+    -- for this, but it also runs MOTD and startup scripts from disks and the filesystem root, so we
+    -- have to monkey-patch `settings.get` to disable that behavior.
+    local overrides = {
         ["shell.allow_startup"] = false,
         ["shell.allow_disk_startup"] = false,
+        ["motd.enable"] = false,
     }
-    -- Leave MOTD when starting the shell normally.
-    if program ~= nil then
-        one_time_overrides["motd.enable"] = false
-    end
-    os.run(
-        {
-            _G = setmetatable({
-                settings = setmetatable({
-                    get = function(name, ...)
-                        if one_time_overrides[name] ~= nil then
-                            local value = one_time_overrides[name]
-                            one_time_overrides[name] = nil
-                            return value
-                        end
-                        return settings.get(name, ...)
-                    end,
-                }, { __index = settings }),
-            }, { __index = _G }),
-
-            -- This is breathtakingly idiotic, but I have no idea what else to do. CC's shell
-            -- does not allow the prompt to be configured in any way, and it's really easy to
-            -- get lost if the hostname is not mentioned, and this is the only place where we
-            -- can inject our logic.
-            write = function(text)
-                local info = debug.getinfo(2)
-                -- Notably, if rsh sessions are nested, only the innermost one will see `write`
-                -- being called from `show_prompt` -- others will see the call to `write` from this
-                -- function instead.
-                if info.source == "@/rom/programs/shell.lua" and info.name == "show_prompt" then
-                    local color = term.getTextColor()
-                    term.setTextColor(colors.green)
-                    write(named.hostname() .. " ")
-                    term.setTextColor(color)
+    os.run({
+        shell = nested_shell,
+        -- `startup.lua` requires various built-in modules; this is close enough.
+        require = require,
+        settings = setmetatable({
+            get = function(name, ...)
+                if overrides[name] ~= nil then
+                    return overrides[name]
                 end
-                write(text)
+                return settings.get(name, ...)
             end,
-        },
-        "rom/programs/shell.lua",
-        table.unpack(command)
-    )
-end
-
-if arg[1] == "--serve" then
-    shell.execute("rom/startup.lua")
-    shell.execute(table.unpack(arg, 2))
-    return
+        }, { __index = settings }),
+    }, "rom/startup.lua")
+    -- Since `startup.lua` overrides path, we have to inject the combined /bin back.
+    nested_shell.setPath("/" .. svc.getCombinedBinPath() .. ":" .. nested_shell.path())
+    nested_shell.execute(program or "msh", ...)
 end
 
 local function serveSession(client_id, params, event_queue)
