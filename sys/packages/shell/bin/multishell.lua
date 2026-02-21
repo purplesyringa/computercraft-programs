f9eacc14a99ba85e2e777f561e8d9c0f91245c4b
Yuki Sireneva <yuki.utk8g@gmail.com>
2026-02-21 18:13:15 +0300

    multishell: add next/prev tab keybind

--- sys/packages/shell/bin/multishell.lua
+++ sys/packages/shell/bin/multishell.lua
@@ -1,4 +1,8 @@
 local first_run = true
+
+local nested
+local shift_held = false
+
 os.run({
     shell = shell,
     os = setmetatable({
@@ -8,6 +12,46 @@ os.run({
                 path = shell.resolveProgram("msh")
             end
             return os.run(env, path, ...)
+        end,
+
+        pullEventRaw = function(filter)
+            if not nested then
+                local i, k, v = 1
+                repeat
+                    k, v = debug.getlocal(2, i)
+                    i = i + 1
+                until k == nil or k == "multishell"
+                assert(k)
+                nested = v
+            end
+
+            while true do
+                local event = table.pack(os.pullEventRaw())
+
+                if (event[1] == "key" or event[1] == "key_up") and (event[2] == keys.leftShift or event[2] == keys.rightShift) then
+                    shift_held = event[1] == "key"
+                end
+
+                if event[1] == "key" and event[2] == keys.pause then
+                    local n = nested.getCount()
+                    local i = nested.getFocus()
+                    if shift_held then
+                        if i == 1 then
+                            nested.setFocus(n)
+                        else
+                            nested.setFocus(i - 1)
+                        end
+                    else
+                        if i == n then
+                            nested.setFocus(1)
+                        else
+                            nested.setFocus(i + 1)
+                        end
+                    end
+                elseif not filter or event[1] == filter or event[1] == "terminate" then
+                    return table.unpack(event)
+                end
+            end
         end,
     }, { __index = os }),
 }, "/rom/programs/advanced/multishell.lua")
