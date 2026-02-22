cc3da615bc31271a867bcdfa78fd8bc870411297
Alisa Sireneva <me@purplesyringa.moe>
2026-02-22 08:35:29 +0300

    pack: Focus on loading code from string

--- sys/packages/pack/init.lua
+++ sys/packages/pack/init.lua
@@ -1,6 +1,4 @@
-local function populate_dependencies(modules, name)
-    if modules[name] then return end
-
+local function loadModuleFromFile(modules, name)
     local path, err = package.searchpath(name, package.path)
     if not path then
         error("Could not find '" .. name .. "': " .. err)
@@ -16,28 +14,33 @@ local function populate_dependencies(modules, name)
     local code = file.readAll()
     file.close()
     modules[name] = code
+end
 
+local function populateDependenciesFromCode(modules, code)
     -- We don't exactly want to parse Lua code here. CC has a Lua parser under `cc.internal`, but
     -- it's not guaranteed to stay there, so I'd rather not use it. Pattern-matching is fine in the
     -- meantime for our own code.
-
     for dep in code:gmatch('require%s*%(?%s*"([^"]+)"') do
-        populate_dependencies(modules, dep)
+        if not modules[dep] then
+            loadModuleFromFile(modules, dep)
+            populateDependenciesFromCode(modules, modules[dep])
+        end
     end
 end
 
-local function pack(entry_name)
-    local modules = {}
-    populate_dependencies(modules, entry_name)
-
+local function formatModules(modules)
     local out = "package.preload = {\n"
     for name, code in pairs(modules) do
         out = out .. ('\t[%q] = load(%q, %q, "t", _ENV),\n'):format(name, code, "=pack:" .. name)
     end
     out = out .. '}\n'
-    out = out .. ('require(%q)\n'):format(entry_name)
-
     return out
 end
 
-return { pack = pack }
+local function packString(code)
+    local modules = {}
+    populateDependenciesFromCode(modules, code)
+    return formatModules(modules) .. code
+end
+
+return { packString = packString }
