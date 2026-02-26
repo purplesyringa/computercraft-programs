01aa19caa1b0976b2fbad9eb150aedf9e81c5d69
Alisa Sireneva <me@purplesyringa.moe>
2026-02-26 18:02:26 +0300

    initrd: Speed up decompression a bit
    This inlines the calculation of counts and pos_in_char into the first
    pass.

--- initrd/snippets.py
+++ initrd/snippets.py
@@ -2,23 +2,27 @@ import re
 from .minify import minify
 from .ser import serialize
 
-decompress_mtf = minify("decompress_mtf", """
+decompress = minify("decompress", """
     local compressed = ...
-
-    local repetitions = {}
-    for c = 0, 255 do
-        repetitions[c] = { c, n = 1 }
-    end
 
     local byte_cache = {}
     for c = 0, 255 do
         byte_cache[c + 1] = c
     end
+
     local rle = 1
+
     local bwt = {}
-    local bwt_pos = 1
+    local bwt_pos = 0
+    local counts = {}
+    for c = -1, 255 do
+        counts[c] = 0
+    end
+    local pos_in_char = {}
+
     local bit_pos = 8
     local symbol
+
     while bit_pos < LIMIT do
         local bits = bit32.lshift(
             string.unpack(">I", compressed, bit32.rshift(bit_pos, 3)),
@@ -29,32 +33,49 @@ decompress_mtf = minify("decompress_mtf", """
             rle = rle * 2 + symbol
         else
             if rle > 1 then
+                local byte = byte_cache[1]
                 local count = rle - 1
-                local r = repetitions[byte_cache[1]]
-                while r.n < count do
-                    table.move(r, 1, r.n, r.n + 1)
-                    r.n = r.n * 2
+                local last_count = counts[byte]
+                for i = 1, count do
+                    bwt[bwt_pos + i] = byte
+                    pos_in_char[bwt_pos + i] = last_count + i
                 end
-                table.move(r, 1, count, bwt_pos, bwt)
+                counts[byte] = last_count + count
                 bwt_pos = bwt_pos + count
                 rle = 1
             end
+            bwt_pos = bwt_pos + 1
             local byte = byte_cache[symbol]
             table.move(byte_cache, 1, symbol - 1, 2)
             byte_cache[1] = byte
             bwt[bwt_pos] = byte
-            bwt_pos = bwt_pos + 1
+            local new_count = counts[byte] + 1
+            counts[byte] = new_count
+            pos_in_char[bwt_pos] = new_count
         end
     end
-    bwt[bwt_pos - 1] = nil
-    return bwt
+    counts[bwt[bwt_pos]] = counts[bwt[bwt_pos]] - 1
+    bwt[bwt_pos] = nil
+
+    for c = 1, 255 do
+        counts[c] = counts[c] + counts[c - 1]
+    end
+    table.move(counts, -1, 254, 0)
+    local s = { [#bwt] = 0 }
+    local pos = SHIFT
+    for i = 1, #bwt do
+        local c = bwt[pos]
+        s[i] = c
+        pos = pos_in_char[pos] + counts[c]
+    end
+    return string.char(table.unpack(s))
 """)
 
-bits = re.search(r"local (\w+)=bit32.lshift", decompress_mtf)[1]
-symbol = re.search(r"if (\w+)<2", decompress_mtf)[1]
-bit_pos = re.search(r"local (\w+)=8", decompress_mtf)[1]
-decompress_mtf1, decompress_mtf2 = decompress_mtf.split("DECODE_SYMBOL()")
-decompress_mtf2 = " " + decompress_mtf2 # for concatenation with generated code
+bits = re.search(r"local (\w+)=bit32.lshift", decompress)[1]
+symbol = re.search(r"if (\w+)<2", decompress)[1]
+bit_pos = re.search(r"local (\w+)=8", decompress)[1]
+decompress1, decompress2 = decompress.split("DECODE_SYMBOL()")
+decompress2 = " " + decompress2 # for concatenation with generated code
 
 code_template = minify("code_template", """
     local compressed = DATA
@@ -70,31 +91,7 @@ code_template = minify("code_template", """
         end
     end
 
-    local bwt = load(DECOMPRESS_MTF1 .. genCode(tree, 0, 0) .. DECOMPRESS_MTF2)(compressed)
-
-    local counts = {}
-    for c = -1, 255 do
-        counts[c] = 0
-    end
-    local pos_in_char = { [#bwt] = 0 }
-    for i, c in ipairs(bwt) do
-        local new_count = counts[c] + 1
-        counts[c] = new_count
-        pos_in_char[i] = new_count
-    end
-    for c = 1, 255 do
-        counts[c] = counts[c] + counts[c - 1]
-    end
-    table.move(counts, -1, 254, 0)
-    local s = { [#bwt] = 0 }
-    local pos = SHIFT
-    for i = 1, #bwt do
-        local c = bwt[pos]
-        s[i] = c
-        pos = pos_in_char[pos] + counts[c]
-    end
-    s = string.char(table.unpack(s))
-
+    local s = load(DECOMPRESS1 .. genCode(tree, 0, 0) .. DECOMPRESS2)(compressed)
     return load(s, "initrd", nil, _ENV)()
 """)
 
@@ -104,8 +101,8 @@ code_template = (
         .replace("BIT_POS", bit_pos)
         .replace("BITS", bits)
         .encode()
-        .replace(b"DECOMPRESS_MTF1", serialize(decompress_mtf1))
-        .replace(b"DECOMPRESS_MTF2", serialize(decompress_mtf2))
+        .replace(b"DECOMPRESS1", serialize(decompress1))
+        .replace(b"DECOMPRESS2", serialize(decompress2))
 )
 
 def generate_sfx(data: bytes, tree: object, total_bit_len: int, shift: int) -> bytes:
