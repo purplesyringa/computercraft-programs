77a7bef5908f285022bc35939d6aeff913ac8b7b
Alisa Sireneva <me@purplesyringa.moe>
2026-03-01 17:36:06 +0300

    getty: Add example external service

--- sys/packages/getty/bin/getty.lua
+++ sys/packages/getty/bin/getty.lua
@@ -3,7 +3,7 @@ local svc = require "svc"
 
 local args = { ... }
 if #args < 2 then
-    print("Usage: getty <monitor-name>/<keyboard-name> <command...>")
+    print("Usage: getty <monitor-id>/<keyboard-id> <command...>")
     print("Use 'default' as the peripheral name to use the built-in monitor/keyboard.")
     return
 end
@@ -13,23 +13,25 @@ local command = { table.unpack(args, 2) }
 if seat_def == "default" then
     seat_def = "default/default"
 end
-local monitor_name, keyboard_name = seat_def:match("^([^/]+)/([^/]+)$")
-assert(monitor_name, "Invalid seat definition '" .. seat_def .. "'")
+local monitor_id, keyboard_id = seat_def:match("^([^/]+)/([^/]+)$")
+assert(monitor_id, "Invalid seat definition '" .. seat_def .. "'")
 
+local monitor_name
 local monitor
-if monitor_name == "default" then
+if monitor_id == "default" then
+    monitor_name = nil
     monitor = term.native()
 else
+    monitor_name = "monitor_" .. monitor_id
     monitor = peripheral.wrap(monitor_name)
     assert(monitor, "No monitor named '" .. monitor_name .. "'")
 end
 
-local function matchesMonitor(name)
-    return (name or "default") == monitor_name
-end
-
-local function matchesKeyboard(name)
-    return (name or "default") == keyboard_name
+local keyboard_name
+if keyboard_id == "default" then
+    keyboard_name = nil
+else
+    keyboard_name = "keyboard_" .. keyboard_id
 end
 
 local bg_command = redirect.runWithEventSource(redirect.runWithTerm, monitor, function()
@@ -53,34 +55,34 @@ while not bg_command.isDead() do
     -- events, so the confusion doesn't arise and we can deliver both the original event and the
     -- rewritten event.
     if event[1] == "char" or event[1] == "paste" then
-        if matchesKeyboard(event[3]) then
+        if keyboard_name == event[3] then
             deliver()
         end
     elseif event[1] == "key" then
-        if matchesKeyboard(event[4]) then
+        if keyboard_name == event[4] then
             if event[2] == keys.leftShift or event[2] == keys.rightShift then
                 shift_pressed = true
             end
             deliver()
         end
     elseif event[1] == "key_up" then
-        if matchesKeyboard(event[3]) then
+        if keyboard_name == event[3] then
             if event[2] == keys.leftShift or event[2] == keys.rightShift then
                 shift_pressed = false
             end
             deliver()
         end
     elseif event[1] == "monitor_resize" then
-        if matchesMonitor(event[2]) then
+        if monitor_name == event[2] then
             bg_command.pushEvent("term_resize")
         end
         deliver()
     elseif event[1] == "term_resize" then
-        if matchesMonitor(nil) then
+        if monitor_name == nil then
             deliver()
         end
     elseif event[1] == "monitor_touch" then
-        if matchesMonitor(event[2]) then
+        if monitor_name == event[2] then
             local button = 1
             if shift_pressed then
                 button = 2
@@ -95,14 +97,14 @@ while not bg_command.isDead() do
         or event[1] == "mouse_scroll"
         or event[1] == "mouse_up"
     ) then
-        if matchesMonitor(nil) then
+        if monitor_name == nil then
             deliver()
         end
     elseif event[1] == "terminate" then
         -- e.g. hangup
         deliver()
     elseif event[1] == "fg_terminate" then
-        if matchesMonitor(event[2]) then
+        if monitor_name == event[2] then
             bg_command.pushEvent("terminate")
         end
     else
---
+++ sys/packages/getty/services/getty-external.lua
@@ -0,0 +1,5 @@
+return {
+    description = "Shell on external seat",
+    type = "process",
+    command = { "getty", "3/1", "multishell" },
+}
