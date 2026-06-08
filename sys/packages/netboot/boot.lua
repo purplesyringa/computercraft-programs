0ac9b018dfad0a8b1e73c65fdbfedfac6c51ac27
Yuki Sireneva <sylfn@disroot.org>
2026-06-08 21:04:47 +0000

    netboot: watch for ender modems

--- sys/packages/netboot/boot.lua
+++ sys/packages/netboot/boot.lua
@@ -1,5 +1,16 @@
 print("Booting from network...")
-peripheral.find("modem", rednet.open)
-rednet.broadcast(nil, "netboot-request")
-local _, payload, _ = rednet.receive("netboot-response")
+local _, payload, side
+parallel.waitForAny(function()
+    peripheral.find("modem", function(side)
+        pcall(rednet.open, side)
+    end)
+    while true do
+        rednet.broadcast(nil, "netboot-request")
+        repeat
+            _, side = os.pullEvent("peripheral")
+        until pcall(rednet.open, side)
+    end
+end, function()
+    _, payload, _ = rednet.receive("netboot-response")
+end)
 load(payload, "=netboot", nil, _ENV)()
