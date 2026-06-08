25a9ed7ddd390e2104578cb356be56fe89cbf6e4
Yuki Sireneva <sylfn@disroot.org>
2026-06-08 02:07:05 +0000

    Fix bg and fg

--- sys/packages/msh/bin/bg.lua
+++ sys/packages/msh/bin/bg.lua
@@ -1,6 +1,6 @@
 local args = { ... }
 if #args ~= 0 then
-    shell.openTab(table.unpack(tArgs))
+    shell.openTab(table.unpack(args))
 else
     shell.openTab("msh")
 end
--- sys/packages/msh/bin/fg.lua
+++ sys/packages/msh/bin/fg.lua
@@ -1,7 +1,7 @@
 local args = { ... }
 local tab
 if #args ~= 0 then
-    tab = shell.openTab(table.unpack(tArgs))
+    tab = shell.openTab(table.unpack(args))
 else
     tab = shell.openTab("msh")
 end
