fe5dded3115297f137a61bc860d8762a8cb81cf9
Yuki Sireneva <sylfn@disroot.org>
2026-06-14 13:45:30 +0000

    "who's online"

--- sys/packages/named/bin/named.lua
+++ sys/packages/named/bin/named.lua
@@ -3,3 +3,12 @@ local ok, own = pcall(named.hostname)
 if ok then
     rednet.host("named", own)
 end
+
+while true do
+    local sender = rednet.receive("named-request")
+    ok, own = pcall(named.hostname)
+    if not ok then
+        own = nil
+    end
+    rednet.send(sender, own, "named-response")
+end
