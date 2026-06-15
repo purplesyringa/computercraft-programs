44e3d52d7dab949c866de152e59d24a1c05ea619
Alisa Sireneva <me@purplesyringa.moe>
2026-06-15 10:42:52 +0300

    svc: luacheck

--- sys/packages/svc/proc.lua
+++ sys/packages/svc/proc.lua
@@ -52,19 +52,6 @@ function proc.list()
     return result
 end
 
-local interactive_events = {
-    terminate = true,
-    char = true,
-    key = true,
-    key_up = true,
-    mouse_click = true,
-    mouse_drag = true,
-    mouse_scroll = true,
-    mouse_up = true,
-    paste = true,
-    term_resize = true,
-}
-
 function proc.loop()
     while true do
         local event = table.pack(os.pullEventRaw())
