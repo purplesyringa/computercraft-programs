6687e1eceddc8a66a2b42e2489467da06c2aaccc
Alisa Sireneva <me@purplesyringa.moe>
2026-06-15 15:55:56 +0300

    named: Populate 'named' namespace step by step

--- sys/packages/named/init.lua
+++ sys/packages/named/init.lua
@@ -3,49 +3,51 @@ settings.define("named.hostname", {
     type = "string",
 })
 
-return {
-    hasHostname = function()
-        return settings.get("named.hostname") ~= nil
-    end,
+local named = {}
 
-    hostname = function()
-        local hostname = settings.get("named.hostname")
-        if hostname == nil then
-            error("Hostname not configured")
-        end
-        return hostname
-    end,
+function named.hasHostname()
+    return settings.get("named.hostname") ~= nil
+end
 
-    setHostname = function(hostname)
-        settings.set("named.hostname", hostname)
-        settings.save()
-        rednet.host("named", hostname)
-    end,
+function named.hostname()
+    local hostname = settings.get("named.hostname")
+    if hostname == nil then
+        error("Hostname not configured")
+    end
+    return hostname
+end
 
-    lookup = function(hostname_or_computer_id)
-        local id = tonumber(hostname_or_computer_id)
-        if id and id >= 0 and id % 1 == 0 then
-            return id
-        end
-        return rednet.lookup("named", hostname_or_computer_id)
-    end,
+function named.setHostname(hostname)
+    settings.set("named.hostname", hostname)
+    settings.save()
+    rednet.host("named", hostname)
+end
 
-    collect = function(pattern, timeout)
-        rednet.broadcast(pattern, "named-request")
+function named.lookup(hostname_or_computer_id)
+    local id = tonumber(hostname_or_computer_id)
+    if id and id >= 0 and id % 1 == 0 then
+        return id
+    end
+    return rednet.lookup("named", hostname_or_computer_id)
+end
 
-        local hosts = {}
-        parallel.waitForAny(function()
-            os.sleep(timeout or 5) -- default
-        end, function()
-            local seen = {}
-            while true do
-                local sender, hostname = rednet.receive("named-response")
-                if not seen[sender] then
-                    seen[sender] = true
-                    table.insert(hosts, { id = sender, hostname = hostname })
-                end
+function named.collect(pattern, timeout)
+    rednet.broadcast(pattern, "named-request")
+
+    local hosts = {}
+    parallel.waitForAny(function()
+        os.sleep(timeout or 5) -- default
+    end, function()
+        local seen = {}
+        while true do
+            local sender, hostname = rednet.receive("named-response")
+            if not seen[sender] then
+                seen[sender] = true
+                table.insert(hosts, { id = sender, hostname = hostname })
             end
-        end)
-        return hosts
-    end,
-}
+        end
+    end)
+    return hosts
+end
+
+return named
