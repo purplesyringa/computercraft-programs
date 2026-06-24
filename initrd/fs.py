259372478da5d1a812d0d9aa122346e19f4c1d28
Yuki Sireneva <sylfn@disroot.org>
2026-06-24 14:22:03 +0000

    initrd: Dedent mounting code in unpacked initrd

--- initrd/fs.py
+++ initrd/fs.py
@@ -29,34 +29,34 @@ def build_uncompressed_initrd(sys_path: Path) -> bytes:
     # `require`/`package` pair, so this won't break anything later on.
     return b"""local tree = TREE
 
-    if mounting then return tree end
+if mounting then return tree end
 
-    local function readFile(path)
-        local ptr = tree
-        for part in path:gmatch("[^/]+") do
-            ptr = ptr and ptr.entries and ptr.entries[part]
-        end
-        return ptr and ptr.contents
+local function readFile(path)
+    local ptr = tree
+    for part in path:gmatch("[^/]+") do
+        ptr = ptr and ptr.entries and ptr.entries[part]
     end
+    return ptr and ptr.contents
+end
 
-    local function loadFromTree(name)
-        local name_as_path = name:gsub("%.", "/")
-        local contents = (
-            readFile("packages/" .. name_as_path .. "/init.lua")
-            or readFile("packages/" .. name_as_path .. ".lua")
-        )
-        if contents then
-            return load(contents, "=initrd:" .. name, nil, _ENV)
-        end
+local function loadFromTree(name)
+    local name_as_path = name:gsub("%.", "/")
+    local contents = (
+        readFile("packages/" .. name_as_path .. "/init.lua")
+        or readFile("packages/" .. name_as_path .. ".lua")
+    )
+    if contents then
+        return load(contents, "=initrd:" .. name, nil, _ENV)
     end
-    table.insert(package.loaders, loadFromTree)
+end
+table.insert(package.loaders, loadFromTree)
 
-    require "vfs.install"
-    local vfs = require "vfs"
-    local tmpfs = require "tmpfs"
-    vfs.unmount("sys")
-    fs.makeDir("sys")
-    tmpfs.mount("sys", tree, true)
-    os._initrd_tree = tree
-    shell.run("sys/startup")
-    """.replace(b"TREE", serialize(tree))
+require "vfs.install"
+local vfs = require "vfs"
+local tmpfs = require "tmpfs"
+vfs.unmount("sys")
+fs.makeDir("sys")
+tmpfs.mount("sys", tree, true)
+os._initrd_tree = tree
+shell.run("sys/startup")
+""".replace(b"TREE", serialize(tree))
