5c3e390eacd83f5e73ba932747a3b421163a2327
Yuki Sireneva <sylfn@disroot.org>
2026-06-25 22:56:46 +0000

    initrd-ng: suffix_array -> libsais
    benchmarking (100x bwt_encode):
    cdivsufsort on duped string: ~40ms
    SuffixArrayConstruction on duped string: ~20ms
    SuffixArrayConstruction on smallest cyclic shift: ~11ms
    BwtConstruction on smallest cyclic shift + manually find primary index: ~10ms
    
    Co-Authored-By: Alisa Sireneva <me@purplesyringa.moe>

--- initrd-ng/initrd-ng/src/bz.rs
+++ initrd-ng/initrd-ng/src/bz.rs
@@ -1,21 +1,59 @@
 use crate::huffman::{Node, huffman_encode};
-use suffix_array::SuffixArray;
+use libsais::BwtConstruction;
+
+// https://web.archive.org/web/20251112195119/http://e-maxx.ru/algo/duval_algorithm
+fn min_cyclic_shift(dup: &[u8], n: usize) -> usize {
+    let mut i = 0;
+    let mut ans = 0;
+    while i < n {
+        ans = i;
+        let mut j = i + 1;
+        let mut k = i;
+        while j < n && dup[k] <= dup[j] {
+            if dup[k] < dup[j] {
+                k = i;
+            } else {
+                k += 1;
+            }
+            j += 1;
+        }
+        while i <= k {
+            i += j - k;
+        }
+    }
+    ans
+}
+
+// This originally used Z-function, but this quadratic loop works faster in practice for our data.
+fn count_cyclic_shifts_less_than_s(dup: &[u8], n: usize) -> usize {
+    (1..n)
+        .filter(|i| {
+            (0..n)
+                .find(|&j| dup[i + j] != dup[j])
+                .is_some_and(|j| dup[i + j] < dup[j])
+        })
+        .count()
+}
 
 fn bwt_encode(s: &[u8]) -> (Vec<u8>, usize) {
+    // This implements cyclic-shift-BWT on top of suffix-BWT provided by libsais. We compute the BWT
+    // string itself by running suffix-BWT on the lexicographically smallest cyclic shift, which
+    // makes it so that comparing suffixes and cyclic shifts produces equivalent results. This
+    // corrupts the primary index (it'll always be computed as 0), so we recompute it directly by
+    // the definition of a suffix array. This method saves 2x time compared to running SA on `dup`
+    // and manually filtering out short suffixes.
     let n = s.len();
-    let mut dup: Vec<u8> = vec![];
+    let mut dup = Vec::with_capacity(2 * s.len());
     dup.extend(s);
     dup.extend(s);
-    // Collect the suffix array, dropping suffixes from the second half
-    let suf = SuffixArray::new(&dup)
-        .into_parts()
-        .1
-        .into_iter()
-        .map(|el| el as usize)
-        .filter(|&el| el < n)
-        .collect::<Vec<_>>();
-    let data = suf.iter().map(|&el| dup[n + el - 1]).collect();
-    let start = suf.iter().position(|&el| el == 0).unwrap();
+    let off = min_cyclic_shift(&dup, n);
+    let data = BwtConstruction::for_text(&dup[off..][..n])
+        .with_owned_temporary_array_buffer32()
+        .single_threaded()
+        .run()
+        .unwrap()
+        .into_vec();
+    let start = count_cyclic_shifts_less_than_s(&dup, n);
     (data, start)
 }
 
