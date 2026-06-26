97f6102f397874ed12223968bc34587ed90105cc
Yuki Sireneva <sylfn@disroot.org>
2026-06-26 12:22:51 +0000

    initrd-ng: Vectorize the entire MTF path (SSSE3)
    ~19.6ms -> ~14.2ms
    
    Co-authored-by: Alisa Sireneva <me@purplesyringa.moe>
    Co-authored-by: Yuki Sireneva <sylfn@disroot.org>

--- initrd-ng/initrd-ng/src/bz.rs
+++ initrd-ng/initrd-ng/src/bz.rs
@@ -57,13 +57,103 @@ fn bwt_encode(s: &[u8]) -> (Vec<u8>, usize) {
     (data, start)
 }
 
-fn mtf_encode(s: &[u8]) -> Vec<u8> {
-    let mut cache: [u8; 256] = core::array::from_fn(|i| i as u8);
-    let mut out = Vec::with_capacity(s.len());
+#[repr(align(16), C)]
+struct Cache([u8; 256]);
+
+#[inline]
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "ssse3")]
+/// # Safety
+///
+/// Requires `cache` to be a permutation
+unsafe fn mtf_single_ssse3(cache: &mut Cache, ch: u8) -> u8 {
+    const SHUFFLES: [__m128i; 16] = {
+        let mut res = [[0u8; 16]; 16];
+        let mut pos = 0;
+        while pos < 16 {
+            res[pos] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+            res[pos].split_at_mut(pos + 1).0.rotate_right(1);
+            pos += 1;
+        }
+        // SAFETY: known ABI
+        unsafe { core::mem::transmute(res) }
+    };
+
+    const SHUFFLES_WITH_CARRY: [__m128i; 16] = {
+        let mut res = SHUFFLES;
+        let mut pos = 0;
+        while pos < 16 {
+            // SAFETY: known ABI
+            unsafe { (&raw mut res[pos]).cast::<u8>().write(0x80) };
+            pos += 1;
+        }
+        res
+    };
+
+    use core::arch::x86_64::*;
+    let mut p: *mut __m128i = cache.0.as_mut_ptr().cast();
+    let ch_broadcast = _mm_set1_epi8(ch.cast_signed());
+
+    let mut chunk = unsafe { p.read() };
+    let eq_mask = _mm_movemask_epi8(_mm_cmpeq_epi8(chunk, ch_broadcast));
+    if eq_mask != 0 {
+        let pos = eq_mask.trailing_zeros() as usize;
+        let new_chunk = _mm_shuffle_epi8(chunk, SHUFFLES[pos]);
+        unsafe { p.write(new_chunk) };
+        return pos as u8;
+    }
+
+    let new_chunk = _mm_or_si128(
+        _mm_shuffle_epi8(chunk, SHUFFLES_WITH_CARRY[15]),
+        _mm_cvtsi32_si128(ch.into()),
+    );
+    unsafe { p.write(new_chunk) };
+
+    loop {
+        let carry = _mm_bsrli_si128(chunk, 15);
+        p = unsafe { p.add(1) };
+
+        chunk = unsafe { p.read() };
+
+        let eq_mask = _mm_movemask_epi8(_mm_cmpeq_epi8(chunk, ch_broadcast));
+        let pos = (eq_mask | 0x8000).trailing_zeros() as usize;
+        let new_chunk = _mm_or_si128(_mm_shuffle_epi8(chunk, SHUFFLES_WITH_CARRY[pos]), carry);
+        unsafe { p.write(new_chunk) };
+
+        if eq_mask != 0 {
+            let offset = unsafe { p.byte_offset_from_unsigned(cache.0.as_mut_ptr()) };
+            return (offset + pos) as u8;
+        }
+    }
+}
+
+#[cfg(target_arch = "x86_64")]
+#[target_feature(enable = "ssse3")]
+/// # Safety
+///
+/// Requires cache to be a permutation
+unsafe fn mtf_encode_ssse3(out: &mut Vec<u8>, cache: &mut Cache, s: &[u8]) {
     for &ch in s {
-        let pos = cache.iter().position(|&c| c == ch).unwrap();
+        // SAFETY: passthrough
+        out.push(unsafe { mtf_single_ssse3(cache, ch) });
+    }
+}
+
+fn mtf_encode(s: &[u8]) -> Vec<u8> {
+    let mut cache: Cache = Cache(core::array::from_fn(|i| i as u8));
+    let mut out = Vec::with_capacity(s.len());
+
+    #[cfg(target_arch = "x86_64")]
+    if std::is_x86_feature_detected!("ssse3") {
+        // SAFETY: ssse3 is detected, cache is an identity permutation
+        unsafe { mtf_encode_ssse3(&mut out, &mut cache, s) };
+        return out;
+    }
+
+    for &ch in s {
+        let pos = cache.0.iter().position(|&c| c == ch).unwrap();
         out.push(pos as u8);
-        cache[0..=pos].rotate_right(1);
+        cache.0[0..=pos].rotate_right(1);
     }
     out
 }
