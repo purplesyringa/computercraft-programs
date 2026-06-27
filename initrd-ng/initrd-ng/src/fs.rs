e32e5d7feb1d65c3fb2812788e41431567d97694
Yuki Sireneva <sylfn@disroot.org>
2026-06-27 20:14:59 +0000

    initrd-ng: make fs::build_tree return rust-native structure

--- initrd-ng/initrd-ng/src/fs.rs
+++ initrd-ng/initrd-ng/src/fs.rs
@@ -1,11 +1,15 @@
 use initrd_core::prelude::*;
-use std::{io::Result, path::Path};
+use std::{collections::HashMap, io::Result, path::Path};
 
-fn build_tree(path: &Path) -> Result<LuaValue> {
+enum Entry {
+    File(Vec<u8>),
+    Dir(HashMap<String, Entry>),
+}
+
+fn build_tree(path: &Path) -> Result<Entry> {
     let meta = path.metadata()?;
-    let mut node = LuaTable::new();
     if meta.is_dir() {
-        let mut entries = LuaTable::new();
+        let mut entries = HashMap::new();
         for dir_entry in std::fs::read_dir(path)? {
             let dir_entry = dir_entry?;
             if dir_entry.metadata()?.is_dir() && dir_entry.path().join(".rdignore").try_exists()? {
@@ -16,28 +20,36 @@ fn build_tree(path: &Path) -> Result<LuaValue> {
                 "Non-ASCII filenames are unsupported: {:?}",
                 dir_entry.file_name()
             );
-            let file_name = dir_entry
-                .file_name()
-                .to_str()
-                .unwrap()
-                .as_bytes()
-                .to_owned();
-            entries.insert(file_name.into(), build_tree(&dir_entry.path())?);
+            let file_name = dir_entry.file_name().to_str().unwrap().to_owned();
+            entries.insert(file_name, build_tree(&dir_entry.path())?);
         }
-        node.insert(b"entries".into(), entries.into());
+        Ok(Entry::Dir(entries))
     } else if meta.is_file() {
-        node.insert(
-            b"contents".into(),
-            LuaString::from(std::fs::read(path)?).into(),
-        );
+        Ok(Entry::File(std::fs::read(path)?))
     } else {
         panic!("Unknown file type {:?} at {path:?}", meta.file_type());
     }
-    Ok(node.into())
+}
+
+fn to_lua_tree(tree: Entry) -> LuaValue {
+    let mut node = LuaTable::new();
+    match tree {
+        Entry::File(contents) => {
+            node.insert(b"contents".into(), LuaString::from(contents).into());
+        }
+        Entry::Dir(entries) => {
+            let lua_entries = entries
+                .into_iter()
+                .map(|(name, entry)| (name.into_bytes().into(), to_lua_tree(entry)))
+                .collect::<LuaTable>();
+            node.insert(b"entries".into(), lua_entries.into());
+        }
+    }
+    node.into()
 }
 
 pub fn build_uncompressed_initrd(sysroot: &Path) -> Vec<u8> {
-    let tree = build_tree(sysroot).unwrap();
+    let tree = to_lua_tree(build_tree(sysroot).unwrap());
     initrd_core::templates::substitute_template(
         include_bytes!("initrd-template.lua"),
         [("__TREE__", &serialize_to_vec(&tree)[..])].into(),
