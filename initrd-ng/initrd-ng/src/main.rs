464966a7a2351234041274412c38f338108937c2
Yuki Sireneva <sylfn@disroot.org>
2026-07-09 04:10:13 +0000

    initrd: analyze: calculate full initrd size in parallel with other sizes

--- initrd-ng/initrd-ng/src/main.rs
+++ initrd-ng/initrd-ng/src/main.rs
@@ -71,25 +71,28 @@ fn main() {
             assert!(aa.dir.as_os_str().is_ascii(), "non-ascii dir name?");
             let mut tree = fs::build_tree(&aa.sysroot).unwrap();
 
-            let total = make_initrd(&tree, false, None).len().cast_signed();
-            println!("{total}\t100.0\t<initrd>");
-
             let names = tree
                 .walk_to(&aa.dir)
                 .expect("no such directory")
                 .iter()
                 .map(|(name, entry)| format!("{name}{}", if entry.is_dir() { "/" } else { "" }))
                 .chain(Some("./".into()))
+                .map(|name| (Some(aa.dir.join(&name)), name))
+                .chain(Some((None, "<initrd>".into())))
                 .collect::<Vec<_>>();
 
             let mut sizes = names
                 .into_par_iter()
-                .map(|name| {
-                    let path = aa.dir.join(&name);
-                    let size = make_initrd(&tree, false, Some(&path)).len().cast_signed();
+                .map(|(ignore, name)| {
+                    let ignore = ignore.as_deref();
+                    let size = make_initrd(&tree, false, ignore).len().cast_signed();
                     (size, name)
                 })
                 .collect::<Vec<(isize, String)>>();
+
+            // XXX: rayon preserves order https://github.com/rayon-rs/rayon/issues/551
+            let total = sizes.last().unwrap().0;
+            sizes.last_mut().unwrap().0 = 0; // convert "size of" to "size without" for <initrd>
 
             sizes.sort();
             for (size, name) in sizes {
