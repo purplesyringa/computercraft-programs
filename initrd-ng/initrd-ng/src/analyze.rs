c6c23324981ba382ad546a3db2a0536354db69c4
Yuki Sireneva <sylfn@disroot.org>
2026-07-12 06:15:00 +0000

    initrd: analyze: Channel-less version

--- initrd-ng/initrd-ng/src/analyze.rs
+++ initrd-ng/initrd-ng/src/analyze.rs
@@ -15,37 +15,63 @@ use std::{
     cmp::Reverse,
     collections::hash_map::HashMap,
     path::{Path, PathBuf},
-    sync::mpsc,
 };
-
-// XXX: Replace with `std::sync::oneshot` when it is stable
-use std::sync::mpsc as oneshot;
 
 fn initrd_size(tree: &fs::Entry, ignore: Option<&Path>) -> isize {
     fs::make_initrd(tree, false, ignore).len().cast_signed()
 }
 
-fn process_dir(total: isize, tree: &fs::Entry, dir: &Path) -> (isize, Vec<(isize, String, bool)>) {
-    let names = tree
-        .walk_to(dir)
-        .expect("no such directory")
-        .iter()
-        .map(|(name, entry)| (Some(dir.join(name)), name.clone(), entry.is_dir()))
-        .collect::<Vec<_>>();
+struct Analyzer {
+    tree: fs::Entry,
+    total: isize,
+}
 
-    let (cur_total, mut sizes) = rayon::join(
-        || total - initrd_size(tree, Some(dir)),
-        || {
-            names
-                .into_par_iter()
-                .map(|(ignore, name, is_dir)| {
-                    (total - initrd_size(tree, ignore.as_deref()), name, is_dir)
-                })
-                .collect::<Vec<_>>()
-        },
-    );
-    sizes.sort_by(|(i1, n1, _), (i2, n2, _)| (Reverse(i1), n1).cmp(&(Reverse(i2), n2)));
-    (cur_total, sizes)
+struct AnalyzedDirectory {
+    this_total: isize,
+    impacts: Vec<(isize, String, bool)>,
+}
+
+impl Analyzer {
+    fn new(sysroot: &Path) -> Self {
+        let tree = fs::build_tree(sysroot).unwrap();
+        let total = initrd_size(&tree, None);
+        Self { tree, total }
+    }
+
+    fn impact(&self, dir: &Path) -> isize {
+        self.total - initrd_size(&self.tree, Some(dir))
+    }
+
+    fn analyze(&self, dir: &Path) -> AnalyzedDirectory {
+        let names = self
+            .tree
+            .walk_to(dir)
+            .expect("no such directory")
+            .iter()
+            .map(|(name, entry)| (dir.join(name), name.clone(), entry.is_dir()))
+            .collect::<Vec<_>>();
+
+        let (this_total, mut impacts) = rayon::join(
+            || self.impact(dir),
+            || {
+                names
+                    .into_par_iter()
+                    .map(|(path, name, is_dir)| (self.impact(&path), name, is_dir))
+                    .collect::<Vec<_>>()
+            },
+        );
+        impacts.sort_by(|(i1, n1, _), (i2, n2, _)| (Reverse(i1), n1).cmp(&(Reverse(i2), n2)));
+
+        AnalyzedDirectory {
+            this_total,
+            impacts,
+        }
+    }
+}
+
+struct Controller {
+    analyzer: Analyzer,
+    cache: HashMap<PathBuf, AnalyzedDirectory>,
 }
 
 fn fractional_percent(value: usize, (min, max): (usize, usize)) -> String {
@@ -72,58 +98,71 @@ fn dismissable_view(view: impl View, has_dismiss: bool, title: String) -> impl V
     view
 }
 
-fn impacts_view(
-    dir: PathBuf,
-    tx: &mpsc::Sender<(PathBuf, oneshot::Sender<Box<dyn View>>)>,
-    total: isize,
-    this_total: isize,
-    impacts: &[(isize, String, bool)],
-) -> impl View {
-    let mut layout = LinearLayout::vertical();
-
-    for (impact, name, is_dir) in impacts {
-        let mut inner = LinearLayout::horizontal();
-
-        inner.add_child(
-            TextView::new(format!("{impact}"))
-                .h_align(HAlign::Right)
-                .fixed_width(8),
-        );
-        inner.add_child(DummyView.fixed_width(1));
-
-        inner.add_child(impact_view(total, *impact));
-        inner.add_child(DummyView.fixed_width(1));
-
-        inner.add_child(impact_view(this_total, *impact));
-        inner.add_child(DummyView.fixed_width(1));
-
-        if *is_dir {
-            let tx = tx.clone();
-            let new_dir = dir.join(name);
-            inner.add_child(Button::new(name, move |siv| {
-                let (otx, orx) = oneshot::channel();
-                tx.send((new_dir.clone(), otx)).unwrap();
-                siv.add_fullscreen_layer(orx.recv().unwrap());
-            }));
-        } else {
-            inner.add_child(TextView::new(name));
+impl Controller {
+    fn new(sysroot: &Path) -> Self {
+        Self {
+            analyzer: Analyzer::new(sysroot),
+            cache: HashMap::new(),
         }
-
-        layout.add_child(inner);
     }
 
-    dismissable_view(
-        ScrollView::new(layout),
-        dir.parent().is_some(),
-        format!("{} ({this_total}/{total})", dir.into_string().unwrap()),
-    )
-    .full_screen()
+    fn impacts_view(&mut self, dir: impl AsRef<Path>) -> impl View {
+        let dir = dir.as_ref();
+
+        let res = self
+            .cache
+            .entry(dir.into())
+            .or_insert_with(|| self.analyzer.analyze(dir));
+
+        let mut layout = LinearLayout::vertical();
+
+        for (impact, name, is_dir) in &res.impacts {
+            let mut inner = LinearLayout::horizontal();
+
+            inner.add_child(
+                TextView::new(format!("{impact}"))
+                    .h_align(HAlign::Right)
+                    .fixed_width(8),
+            );
+            inner.add_child(DummyView.fixed_width(1));
+
+            inner.add_child(impact_view(self.analyzer.total, *impact));
+            inner.add_child(DummyView.fixed_width(1));
+
+            inner.add_child(impact_view(res.this_total, *impact));
+            inner.add_child(DummyView.fixed_width(1));
+
+            if *is_dir {
+                let new_dir = dir.join(name);
+                inner.add_child(Button::new(name, move |siv| {
+                    let layer = siv
+                        .user_data::<Controller>()
+                        .unwrap()
+                        .impacts_view(&new_dir);
+                    siv.add_fullscreen_layer(layer);
+                }));
+            } else {
+                inner.add_child(TextView::new(name));
+            }
+
+            layout.add_child(inner);
+        }
+
+        dismissable_view(
+            ScrollView::new(layout),
+            dir.parent().is_some(),
+            format!(
+                "{} ({}/{})",
+                dir.to_str().unwrap(),
+                res.this_total,
+                self.analyzer.total
+            ),
+        )
+        .full_screen()
+    }
 }
 
 pub fn analyze(sysroot: &Path) {
-    let tree = fs::build_tree(sysroot).unwrap();
-    let total = initrd_size(&tree, None);
-
     let mut siv = Cursive::new();
 
     siv.set_theme(Theme {
@@ -151,23 +190,10 @@ pub fn analyze(sysroot: &Path) {
         },
     });
 
-    let (tx, rx) = mpsc::channel::<(PathBuf, _)>();
+    siv.set_user_data(Controller::new(sysroot));
 
-    let new_tx = tx.clone();
-    rayon::spawn(move || {
-        let mut cache = HashMap::new();
-        for (dir, otx) in rx {
-            let (cur_impact, impacts) = cache
-                .entry(dir.clone())
-                .or_insert_with(|| process_dir(total, &tree, &dir));
-            let view = impacts_view(dir, &new_tx, total, *cur_impact, impacts);
-            otx.send(Box::new(view)).unwrap();
-        }
-    });
-
-    let (otx, orx) = oneshot::channel();
-    tx.send(("/".into(), otx)).unwrap();
-    siv.add_fullscreen_layer(orx.recv().unwrap());
+    let layer = siv.user_data::<Controller>().unwrap().impacts_view("/");
+    siv.add_fullscreen_layer(layer);
 
     siv.add_global_callback('q', |s| s.quit());
 
